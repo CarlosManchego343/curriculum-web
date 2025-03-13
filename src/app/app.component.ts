04287900d6c4245f84d76d8051dc51a3af88ffef
Carlos Mario Manchego Pabon <carlosmariomanchego1@gmail.com>
2025-03-12 19:06:57 -0500

    Feature-Components carpet and standalone-CMMP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PresentacionComponent } from './componentes/presentacion/presentacion.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, PresentacionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
