c8b0beb8f54f4da3e6843b36f2badfa41b6782b0
Carlos Mario Manchego Pabon <carlosmariomanchego1@gmail.com>
2025-03-31 21:11:08 -0500

    Feature-Decoration and info in the presetation slide-CMMP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PresentacionComponent } from './componentes/presentacion/presentacion.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PresentacionComponent],
+  imports: [RouterOutlet, PresentacionComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
