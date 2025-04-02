923a1c3c203e1d2c2161050b605a0a94c677d770
Carlos Mario Manchego Pabon <carlosmariomanchego1@gmail.com>
2025-04-01 20:37:21 -0500

    Update-Presentation shape and recolorize the main page-CMMP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PresentacionComponent } from './componentes/presentacion/presentacion.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { Title } from '@angular/platform-browser';
 
 
 @Component({
@@ -11,6 +12,13 @@ import { NavbarComponent } from './componentes/navbar/navbar.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private titleService: Title) { }
+
+  ngOnInit() {
+    this.titleService.setTitle('Mi Portafolio Profesional | Carlos Manchego');
+  }
+  
   title = 'Titulo facil de leer';
 }
