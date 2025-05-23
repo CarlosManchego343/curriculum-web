624e34ca605d69fcbe173f3e4bf4b515ad7dce1c
Carlos Mario Manchego Pabon <carlosmariomanchego1@gmail.com>
2025-05-23 14:33:44 -0500

    Feature-Lenguajes parragraph-CMMP

--- src/app/componentes/lenguajes/lenguajes.component.ts
+++ src/app/componentes/lenguajes/lenguajes.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lenguajes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lenguajes.component.html',
   styleUrl: './lenguajes.component.css'
 })
 export class LenguajesComponent {
-
+ lenguajesConocidos = [
+  "Java",
+  "PHP",
+  "Python",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "Dart",
+  "Spring Boot",
+  "Bootstrap",
+  "React",
+  "React Native",
+  "Angular",
+  "Flutter"
+ ]
 }
